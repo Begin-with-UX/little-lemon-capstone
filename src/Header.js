69cec63777a6bd58286af1dd111f9db27d3e6ec3
Anthony Begin <tony@Anthonys-MacBook-Pro.local>
2026-03-12 16:41:02 -0400

    styled html elelments with css

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import logo from "./Logo.svg";
+
+function Header() {
+  return (
+    <header>
+      <img src={logo} alt="Little Lemon Logo" />
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/Main.js
@@ -0,0 +1,12 @@
+function Main() {
+  return (
+    <main>
+      <h1>Little Lemon</h1>
+      <h2>Chicago</h2>
+      <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+      <span>Reserve a Table</span>
+    </main>
+  );
+}
+
+export default Main;
